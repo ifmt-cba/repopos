e5ff3662a6a71e3f84ae86e1209220411e2a0f50
Preti, João Paulo <108187064+preti-joao@users.noreply.github.com>
2024-05-07 21:11:50 -0400

    feat: exemplo de geração de jwt com Go

---
+++ jwt.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var secretKey = []byte("secret-key")
+
+func createToken(username string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"username": username,
+			"exp":      time.Now().Add(time.Hour * 24).Unix(),
+		})
+
+	tokenString, err := token.SignedString(secretKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func main() {
+	println(createToken("preti.joao"))
+}
